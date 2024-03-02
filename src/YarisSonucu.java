e110dae36ff770dabc9883d5c4bf43f2ab375a9a
Hexcha <tanerayranci@yandex.com>
2024-03-02 20:56:41 +0300

    First Commit
    First Date: 02-03-2024

---
+++ src/YarisSonucu.java
@@ -0,0 +1,80 @@
+public class YarisSonucu {
+
+    private final String[] names;
+    private final int[] minutes;
+
+    public YarisSonucu(String[] names,int[] minutes) {
+        this.names = names;
+        this.minutes=minutes;
+    }
+
+    public int fastest(){
+        int min=minutes[0];
+        for (int minute : minutes) {
+            if (minute < min)
+                min = minute;
+        }
+        for (int index = 0; index < minutes.length; index++) {
+            if (minutes[index] == min) {
+                return index;
+            }
+        }
+        return -1;
+
+    }
+    public int secondFastest(){
+        int min=minutes[0];
+        for (int minute : minutes) {
+            if (minute < min && minute!=minutes[fastest()])
+                min = minute;
+        }
+        for (int index = 0; index < minutes.length; index++) {
+            if (minutes[index] == min) {
+                return index;
+            }
+        }
+        return -1;
+
+    }
+    public int thirdFastest(){
+        int min=minutes[0];
+        for (int minute : minutes) {
+            if (minute < min && minute!=minutes[fastest()] && minute!=minutes[secondFastest()])
+                min = minute;
+        }
+        for (int index = 0; index < minutes.length; index++) {
+            if (minutes[index] == min) {
+                return index;
+            }
+        }
+        return -1;
+
+    }
+    public void classes(){
+        int A=0;
+        int B=0;
+        int C=0;
+        for (int minute:minutes){
+            if(199<minute && minute<300)
+                A++;
+            else if (299<minute && minute<400)
+                B++;
+            else if(399<minute)
+                C++;
+                
+
+        }
+        System.out.println("A -> "+ A);
+        System.out.println("B -> "+ B);
+        System.out.println("C -> "+ C);
+
+    }
+    public void displayWinners(){
+        System.out.println("Birinci: "+names[fastest()]+ " " + minutes[fastest()]+"'");
+        System.out.println("İkinci: "+names[secondFastest()]+ " " + minutes[secondFastest()]+"'");
+        System.out.println("Üçüncü: "+names[thirdFastest()]+ " " + minutes[thirdFastest()]+"'");
+
+    }
+
+
+}
